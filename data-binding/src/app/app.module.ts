6fca0201e1bdbcbe694ffd59ca68ded22682b6c8
eibrunodev <eibrunodev@gmail.com>
2023-06-21 23:10:51 -0300

    Atualiza input propeties

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
-
+import { FormsModule } from '@angular/forms';
+import { InputPropertyComponent } from './input-property/input-property.component';
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    InputPropertyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -15,7 +15,58 @@ export class DataBindingComponent {
     return 1;
   }
 
+  clique(){
+    alert("Clicado!!")
+  }
+
+  valorAtual: string ='';
+  valorSalvo: string= '';
+
+  isMouseOver: boolean = false;
+
+  OnKeyUp(event: KeyboardEvent){
+    this.valorAtual=(<HTMLInputElement>event.target).value;
+  }
+
+  salvarValor(valor:string){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOnOver(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  clickAqui(){
+    alert("Clicou em mim em Bruno!!")
+  }
+
+  varInputValue: string = '';
+  salvaValorValue: string = '';
+
+  isMousePassa: boolean = false;
+
+  valorDoInput(event:KeyboardEvent){
+    this.varInputValue = (<HTMLInputElement>event.target).value;
+  }
+
+  totalValue(valor:string){
+    this.salvaValorValue = valor
+  }
+
+  passaSaiMouse(){
+    this.isMousePassa = !this.isMousePassa
+  }
+
   getLikeAngular(){
     return true;
+  }
+
+
+  nome: string= 'Seu nome';
+  nomeDoCurso: string = 'Angular';
+  tipoDoCurso: string = 'Programação'
+  pessoa: any = {
+    nome:"Seu Nome",
+    idade:20
   }
 }
